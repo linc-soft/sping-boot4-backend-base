2703660643f5d486128d1beecba685112ca63f49
宋秀林 <songxiulin@163.com>
2026-04-07 15:43:56 +0900

    chore(build): Add the initial setup and basic structure of the project.
    - Incorporate automatic Google-style formatting using the fmt-maven-plugin
    - Add a Git pre-commit hook and configure a script to automatically format Java code before committing.
    - Add an EditorConfig file to unify the code style.

---
+++ src/main/java/com/lincsoft/Application.java
@@ -0,0 +1,17 @@
+package com.lincsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point of the application
+ *
+ * @author 林创科技
+ * @since 2026-04-07
+ */
+@SpringBootApplication
+public class Application {
+  void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
