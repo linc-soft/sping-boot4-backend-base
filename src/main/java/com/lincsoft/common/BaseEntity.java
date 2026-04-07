864f9186faea5e2de775cbc6cd77852cf9e5bf00
宋秀林 <songxiulin@163.com>
2026-04-07 17:48:27 +0900

    feat(common): Add a base entity class.
    - Defines common fields shared by all entities
    - Implement automatic field completion using MyBatis-Plus annotations (@TableId, @TableField).

---
+++ src/main/java/com/lincsoft/common/BaseEntity.java
@@ -0,0 +1,42 @@
+package com.lincsoft.common;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serial;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * Base entity abstract class.
+ *
+ * <p>Defines common fields that all entities inherit.
+ *
+ * @author 林创科技
+ * @since 2026-04-07
+ */
+@Data
+public abstract class BaseEntity implements Serializable {
+  @Serial private static final long serialVersionUID = 1L;
+
+  /* ID */
+  @TableId(type = IdType.AUTO)
+  private Long id;
+
+  /* Create User */
+  @TableField(fill = FieldFill.INSERT)
+  private String createBy;
+
+  /* Create Time */
+  @TableField(fill = FieldFill.INSERT)
+  private String createAt;
+
+  /* Update User */
+  @TableField(fill = FieldFill.INSERT_UPDATE)
+  private String updateBy;
+
+  /* Update Time */
+  @TableField(fill = FieldFill.INSERT_UPDATE)
+  private String updateAt;
+}
